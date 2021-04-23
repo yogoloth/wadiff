dddbb7a4b3ba0915bb8835bf788e04a09b83d90a
Wang Junling <junling.wang@timanetworks.com>
2021-04-23 17:18:52 +0800

    新文件：   df.go 	修改：     diff_str.go

---
+++ df.go
@@ -0,0 +1,59 @@
+package main
+
+//import (
+//lev "github.com/texttheater/golang-levenshtein/levenshtein"
+//"os"
+//)
+
+//func CreateDf(str_x []rune, str_y []rune) (df [][]int) {
+//	options := lev.Options{
+//		InsCost: 1,
+//		DelCost: 1,
+//		SubCost: 1,
+//		Matches: lev.IdenticalRunes,
+//	}
+//
+//	df = lev.MatrixForStrings(str_x, str_y, options)
+//
+//	debug := false
+//	if debug == true {
+//		lev.WriteMatrix(str_x, str_y, df, os.Stdout)
+//	}
+//	return
+//}
+
+func min(a int, b int) int {
+	if b < a {
+		return b
+	}
+	return a
+}
+
+func CreateDf(source []rune, target []rune) (df [][]int) {
+	//  http://www.let.rug.nl/~kleiweg/lev/levenshtein.html
+	height := len(source) + 1
+	width := len(target) + 1
+	df = make([][]int, height)
+
+	for i := 0; i < height; i++ {
+		df[i] = make([]int, width)
+		df[i][0] = i * 1
+	}
+	for j := 1; j < width; j++ {
+		df[0][j] = j * 1
+	}
+
+	for i := 1; i < height; i++ {
+		for j := 1; j < width; j++ {
+			delCost := df[i-1][j] + 1
+			matchSubCost := df[i-1][j-1]
+			if source[i-1] != target[j-1] {
+				matchSubCost += 1
+			}
+			insCost := df[i][j-1] + 1
+			df[i][j] = min(delCost, min(matchSubCost,
+				insCost))
+		}
+	}
+	return
+}
--- diff_str.go
+++ diff_str.go
@@ -3,13 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gookit/color"
-	lev "github.com/texttheater/golang-levenshtein/levenshtein"
-	"os"
 )
-
-//func getPosition(matrix [][]int, x int, y int) int {
-//	return matrix[x][y]
-//}
 
 func GetPrev(matrix [][]int, x int, y int) (px int, py int) {
 	if x > 1 {
@@ -32,30 +26,21 @@ func GetRoute(source []rune, target []rune, matrix [][]int) (routex []rune, rout
 	x := len(matrix) - 1
 	y := len(matrix[0]) - 1
 
-	//fmt.Println("hello", x)
-	//var xp, yq int
-
 	routex = make([]rune, len(matrix[0])+len(matrix))
 	routey = make([]rune, len(matrix[0])+len(matrix))
 
 	i = 0
 	for {
 		i++
-		//fmt.Println(x, y)
 		if x < 1 && y < 1 {
 			break
 		}
 		px, py := GetPrev(matrix, x, y)
-		//pv := getPosition(matrix, px, py)
-		//v := getPosition(matrix, x, y)
 		if px == x-1 && py == y-1 {
-			//if pv == v {
 			routey[i] = target[y-1]
 			routex[i] = source[x-1]
 			x = px
 			y = py
-			//} else {
-			//}
 		} else if px == x-1 {
 			routey[i] = rune(' ')
 			routex[i] = source[x-1]
@@ -73,7 +58,6 @@ func GetRoute(source []rune, target []rune, matrix [][]int) (routex []rune, rout
 
 func PrintDiff(str1 []rune, str2 []rune, size int) {
 	for i := size; i > 0; i-- {
-		//if routex[i] == routey[i] || routey[i] == rune(' ') {
 		if str1[i] == str2[i] {
 			fmt.Printf("%c", str1[i])
 		} else if str1[i] == rune(' ') {
@@ -87,25 +71,8 @@ func PrintDiff(str1 []rune, str2 []rune, size int) {
 
 func GetDiff(str_1 []rune, str_2 []rune) (routex []rune, routey []rune, route_size int) {
 
-	//str_x := []rune(str_1)
-	//str_y := []rune(str_2)
-	str_x := str_1
-	str_y := str_2
+	matrix := CreateDf(str_1, str_2)
 
-	options := lev.Options{
-		InsCost: 1,
-		DelCost: 1,
-		SubCost: 1,
-		Matches: lev.IdenticalRunes,
-	}
-
-	matrix := lev.MatrixForStrings(str_x, str_y, options)
-
-	debug := false
-	if debug == true {
-		lev.WriteMatrix(str_x, str_y, matrix, os.Stdout)
-	}
-
-	routex, routey, route_size = GetRoute(str_x, str_y, matrix)
+	routex, routey, route_size = GetRoute(str_1, str_2, matrix)
 	return
 }
